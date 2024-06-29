529180db613fe98651a531a424853f2116caf8db
mahbodsr <srmahbod@gmail.com>
2024-06-29 17:28:26 +0330

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const socks5 = require('node-socks5-server');
+
+const users = {
+  'mahbodsr': 'topcs1.6',
+};
+
+const userPassAuthFn = (user, password) => {
+  if (users[user] === password) return true;
+  return false;
+};
+
+const server = socks5.createServer({
+  userPassAuthFn,
+});
+server.listen(10000);
